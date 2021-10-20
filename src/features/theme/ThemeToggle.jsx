251a36e81c174f8064408d1f5fbc490116d37e1d
Alexander Rose <alexander.rose.11@my.csun.edu>
2021-10-20 16:24:04 -0700

    Update ThemeToggle.jsx

--- src/features/theme/ThemeToggle.jsx
+++ src/features/theme/ThemeToggle.jsx
@@ -4,7 +4,6 @@ import {ThemeContext} from "../../App";
 import lightTheme, {darkTheme} from "./theme";
 
 const ThemeToggle = () => {
-    // eslint-disable-next-line no-unused-vars
     const [theme, setTheme] = useContext(ThemeContext)
 
     return (
